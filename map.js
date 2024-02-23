2c77da76b700c8370d6a17355e151abbf7c1111f
unknown <svliyon@gmail.com>
2024-02-23 20:07:06 +1100

    chloropleth and marker map done

--- map.js
+++ map.js
@@ -8,7 +8,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add a marker for a specific location (e.g., Sydney)
-var marker = L.marker([-33.8688, 151.2093]).addTo(map);
+//var marker = L.marker([-33.8688, 151.2093]).addTo(map);
 
 /* // Load the GeoJSON file
 fetch('electoral_boundaries_50p.geojson')
@@ -18,7 +18,7 @@ fetch('electoral_boundaries_50p.geojson')
         L.geoJSON(data).addTo(map);
     }); */
 
-/* // Function to geocode electorates using Nominatim
+/*  // Function to geocode electorates using Nominatim
 function geocodeElectorate(electorate, callback) {
     // Use Nominatim API to geocode the electorate
     fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + electorate)
@@ -34,50 +34,34 @@ function geocodeElectorate(electorate, callback) {
       .catch(error => {
         console.error('Error geocoding electorate:', error);
       });
-  } */
+  }  */
 
-// Function to geocode electorates using L.Highlight
-function geocodeElectorate(electorate, callback) {
-    // Create a new instance of L.Layer.Highlight
-    var highlightLayer = new L.Layer.Highlight();
-
-    // Define the search options
-    var searchOptions = {
-        q: electorate
-    };
-
-    // Use the L.Highlight plugin to search for the specified electorate
-    highlightLayer.do(searchOptions, {
-        eventHandlers: {
-            click: function(area) {
-                // Extract latitude and longitude from the clicked area
-                var latitude = area.latlng.lat;
-                var longitude = area.latlng.lng;
-                
-                // Pass the coordinates to the callback function
-                callback([latitude, longitude]);
-            }
-        }
-    });
-}
+// Define a custom marker icon with a watermelon image
+var watermelonIcon = L.icon({
+  iconUrl: 'watermelon-icon.png', // URL to the watermelon image file
+  iconSize: [40, 40], // Size of the icon
+  iconAnchor: [20, 40], // Point of the icon which corresponds to marker's location
+  popupAnchor: [0, -40] // Point from which the popup should open relative to the iconAnchor
+});
 
 // Parse CSV data and add markers to the map
-Papa.parse('mp_data.csv', {
-    download: true,
-    header: true,
-    complete: function(results) {
-      results.data.forEach(function(row) {
-        // Geocode the electorate to get latitude and longitude
-        var electorate = row.Electorate;
-        geocodeElectorate(electorate, function(coordinates) {
-          // Create a marker with a popup showing MP's information
-          L.marker(coordinates)
-            .bindPopup('<b>' + row['Hon'] + ' ' + row['Surname'] + ', ' + row['First Name'] + '</b><br>' +
-                       'Electorate: ' + row['Electorate'] + '<br>' +
-                       'Political Party: ' + row['Political Party'])
-            .addTo(map);
-        });
-      });
-    }
-  });
-  
+Papa.parse('mp_data_geocoded.csv', {
+  download: true,
+  header: true,
+  complete: function(results) {
+    results.data.forEach(function(row) {
+      if(row.Voted == 'Yes'){
+      // Extract latitude and longitude from the row
+      var latitude = parseFloat(row.Latitude);
+      var longitude = parseFloat(row.Longitude);
+      
+      // Create a marker with a popup showing MP's information
+      L.marker([latitude, longitude], { icon: watermelonIcon })
+        .bindPopup('<b>' + row['Honorific'] + ' ' + row['Surname'] + ', ' + row['First Name'] + '</b><br>' +
+                   'Electorate: ' + row['Electorate'] + '<br>' +
+                   'Political Party: ' + row['Political Party'])
+        .addTo(map);
+      }
+    });
+  }
+});
